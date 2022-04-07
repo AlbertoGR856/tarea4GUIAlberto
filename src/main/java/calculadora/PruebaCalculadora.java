5496ba5049cbcd75904d3118a2ab96525dbda9aa
Alberto <albertogr578@gmail.com>
2022-04-07 15:49:58 +0200

    La clase PruebaCalculadora, que crea el frame donde se incluye un panel del tipo PanelPrincipal.

--- src/main/java/calculadora/PruebaCalculadora.java
+++ src/main/java/calculadora/PruebaCalculadora.java
@@ -5,6 +5,8 @@
  */
 package calculadora;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author GR6
@@ -13,7 +15,19 @@ package calculadora;
 En un nuevo proyecto, llamado tarea4GUI+tunombre y usando el ejemplo de la calculadora que hay en los apuntes, implementa la aplicación para que al menos se puedan realizar las operaciones aritméticas básicas con dos operandos.
 Usa control de versiones en un repositorio público.
 Sube el archivo en formato ZIP y el enlace de github.
-*/
+ */
 public class PruebaCalculadora {
-    
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Calculadora");
+        frame.setBounds(250, 250, 0, 0);
+        frame.add(new PanelPrincipal());
+
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+
+    }
+
 }
