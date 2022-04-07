3d6eb0ca955da34326ee014acb19a5355679cee8
Alberto <albertogr578@gmail.com>
2022-04-07 15:39:16 +0200

    Añade las clases al repositorio

---
+++ src/main/java/calculadora/PruebaCalculadora.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author GR6
+ */
+/*
+En un nuevo proyecto, llamado tarea4GUI+tunombre y usando el ejemplo de la calculadora que hay en los apuntes, implementa la aplicación para que al menos se puedan realizar las operaciones aritméticas básicas con dos operandos.
+Usa control de versiones en un repositorio público.
+Sube el archivo en formato ZIP y el enlace de github.
+*/
+public class PruebaCalculadora {
+    
+}
