b0877cf30ca7883b9ea76f6b03a75c02365d2bba
Alberto <albertogr578@gmail.com>
2022-04-07 15:48:32 +0200

    Se crea la clase PanelPrincipal, que contendrá un panel con los botones y un área de texto donde se verán los números, operaciones y resultados.

--- src/main/java/calculadora/PanelPrincipal.java
+++ src/main/java/calculadora/PanelPrincipal.java
@@ -5,6 +5,11 @@
  */
 package calculadora;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 /**
  *
  * @author GR6
@@ -14,9 +19,35 @@ En un nuevo proyecto, llamado tarea4GUI+tunombre y usando el ejemplo de la calcu
 al menos se puedan realizar las operaciones aritméticas básicas con dos operandos.
 Usa control de versiones en un repositorio público.
 Sube el archivo en formato ZIP y el enlace de github.
-*/
-public class PanelPrincipal {
-    
-    
-    
+ */
+public class PanelPrincipal extends JPanel {
+
+    // Atributos de la clase (privados)
+    private PanelBotones botonera;
+    private JTextArea areaTexto;
+    private int tipoOperacion;
+
+    // Constructor
+    public PanelPrincipal() {
+        initComponents();
+        tipoOperacion = -1; // No hay operaciones en la calculadora
+    }
+
+    // Se inicializan los componentes gráficos y se colocan en el panel
+    private void initComponents() {
+        // Creamos el panel de botones
+        botonera = new PanelBotones();
+        // Creamos el área de texto
+        areaTexto = new JTextArea(10, 50);
+        areaTexto.setEditable(false);
+        areaTexto.setBackground(Color.white);
+
+        //Establecemos layout del panel principal
+        this.setLayout(new BorderLayout());
+        // Colocamos la botonera y el área texto
+        this.add(areaTexto, BorderLayout.NORTH);
+        this.add(botonera, BorderLayout.SOUTH);
+
+    }
+
 }
